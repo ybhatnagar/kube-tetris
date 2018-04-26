aec3c10d134550ce356c18b910635131fcd0b640
Yash <ybhatnagar@vmware.com>
2018-04-26 18:51:58 +0530

    added pivot ratio methhods

--- src/main/java/com/vmware/borathon/WorkloadBalancer.java
+++ src/main/java/com/vmware/borathon/WorkloadBalancer.java
@@ -4,7 +4,36 @@ import java.util.List;
 
 public class WorkloadBalancer {
 
-    public WorkloadBalancer(List<Node> nodes){
+    private List<Node> nodes;
 
+    public static double pivotRatio;
+    public static double smallestEntropy;
+
+    public WorkloadBalancer(MigrationController controller){
+        nodes = controller.getNodes();
+        pivotRatio=getPivotRatio();
+        smallestEntropy=pivotRatio;
     }
+
+    public double getPivotRatio(){
+        long totalAvailableCpu = 0, totalAvailableMem=0;
+
+        for(int i=0;i<nodes.size();i++){
+            totalAvailableCpu += nodes.get(i).getAvailableCapacity().getCpuMillicore();
+            totalAvailableMem += nodes.get(i).getAvailableCapacity().getMemoryMB();
+        }
+        return totalAvailableCpu/totalAvailableMem;
+    }
+
+    public double getEntropy(List<Node> nodes){
+        double sum = 0;
+        for (int i=0;i<nodes.size();i++){
+            sum += Math.abs(pivotRatio-nodes.get(i).getAvailableCapacity().getCpuMemoryRatio());
+        }
+        return sum;
+    }
+
+    
+
+
 }
