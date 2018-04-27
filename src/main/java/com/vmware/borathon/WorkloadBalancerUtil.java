6700344b64c2166c28440fe8abcf9ade47b50890
Yash <ybhatnagar@vmware.com>
2018-04-27 13:21:40 +0530

    workload balancer

--- src/main/java/com/vmware/borathon/WorkloadBalancerUtil.java
+++ src/main/java/com/vmware/borathon/WorkloadBalancerUtil.java
@@ -1,8 +1,13 @@
 package com.vmware.borathon;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 public class WorkloadBalancerUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(WorkloadBalancerUtil.class);
 
     public double getPivotRatio(List<Node> nodes){
         long totalAvailableCpu = 0, totalAvailableMem=0;
@@ -20,5 +25,9 @@ public class WorkloadBalancerUtil {
             sum += Math.abs(pivotRatio - nodes.get(i).getAvailableCapacity().getCpuMemoryRatio());
         }
         return sum;
+    }
+
+    public double getDistanceFromPivot(Node node, double pivotratio){
+        return Math.abs(pivotratio-node.getAvailableCapacity().getCpuMemoryRatio());
     }
 }
