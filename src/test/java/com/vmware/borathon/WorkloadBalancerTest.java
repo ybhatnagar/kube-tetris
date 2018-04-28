03222800d0d0848684f02722f80861471fa0d7b8
Amit Kumar <amit20587@gmail.com>
2018-04-28 19:09:49 +0530

    Adding test without file write

--- src/test/java/com/vmware/borathon/WorkloadBalancerTest.java
+++ src/test/java/com/vmware/borathon/WorkloadBalancerTest.java
@@ -1,11 +1,8 @@
 package com.vmware.borathon;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -19,31 +16,13 @@ import com.vmware.borathon.balancer.WorkLoadBalancerImpl;
 import com.vmware.borathon.balancer.WorkLoadBalancerUtil;
 import com.vmware.borathon.loadsimulator.NodeDataGenerator;
 
-@RunWith(Parameterized.class)
 public class WorkloadBalancerTest {
-    private static final Logger log = LoggerFactory.getLogger(WorkloadBalancerTest.class);
 
     private static SystemController systemController;
 
-    private static int iteration_count;
-
-    private static BufferedWriter writer;
-
-    private static WorkLoadBalancerUtil workLoadBalancerUtil;
-
-    private StringBuilder sb;
-
-    @Parameterized.Parameters
-    public static Object[][] data() {
-        return new Object[100][0];
-    }
-
     @BeforeClass
     public static void setup() throws IOException {
-        writer = new BufferedWriter(new FileWriter("Validate_Algo.txt"));
-        iteration_count = 0;
-        workLoadBalancerUtil = new WorkLoadBalancerUtil();
-        writer.write("Recording results...................\n");
+
     }
 
     @Before
@@ -52,33 +31,11 @@ public class WorkloadBalancerTest {
         systemController = new SystemControllerImpl();
         List<Node> inputNodes = NodeDataGenerator.generate(3, 5);
         inputNodes.forEach(node -> systemController.addNode(node));
-        sb = new StringBuilder();
-        sb.append("===============================================================Iteration : ").append(iteration_count).append("==================================================================\n");
-        sb.append("Nodes information before balancing with entropy : ").append(systemController.getSystemEntropy()).append("\n");
-        systemController.getNodes().forEach(n -> {
-            sb.append(n).append("\n");
-            sb.append(n.getPods()).append("\n");
-        });
-        writer.append(sb.toString());
     }
 
     @Test
     public void scenarioTest() throws Exception{
         WorkLoadBalancer workLoadBalancer = new WorkLoadBalancerImpl(systemController, 5);
         workLoadBalancer.balance();
-        sb = new StringBuilder();
-        sb.append("\nNodes information after balancing with entropy : ").append(systemController.getSystemEntropy()).append("\n");
-        systemController.getNodes().forEach(n -> {
-            sb.append(n).append("\n");
-            sb.append(n.getPods()).append("\n");
-        });
-        writer.append(sb.toString());
-        sb.append("=======================================================================================================================================================================\n");
-        ++iteration_count;
-    }
-
-    @After
-    public void increment() throws Exception{
-
     }
 }
