91e6ada76cff150e28578fe39e7684d9eb237f50
Amit Kumar <cskjha@gmail.com>
2018-04-28 21:10:12 +0530

    Complete working solution

---
+++ src/main/java/com/vmware/borathon/scheduler/helpers/Address.java
@@ -0,0 +1,15 @@
+package com.vmware.borathon.scheduler.helpers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    private String from;
+    private String to;
+}
