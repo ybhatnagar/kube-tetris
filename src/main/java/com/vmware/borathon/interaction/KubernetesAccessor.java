4fa1bb658bab82b626926f452e407a62f7c7f3b6
Amit Kumar <amit20587@gmail.com>
2018-04-30 17:45:29 +0530

    Refactoring changes

--- src/main/java/com/vmware/borathon/interaction/KubernetesAccessor.java
+++ src/main/java/com/vmware/borathon/interaction/KubernetesAccessor.java
@@ -9,9 +9,12 @@ import java.util.Collection;
 import java.util.List;
 
 public interface KubernetesAccessor {
+    List<Node> populateSystem();
     void migratePod(String toRemove, String toNode) throws IllegalStateException, ParseException,InterruptedException;
     List<Node> getSystemSnapshot() throws ParseException;
     void swapPods(String podA, String nodeA, String podB, String nodeB);
     void createPod(String onNode,JSONObject podConfig) throws InterruptedException,ParseException;
     boolean deletePod(String podName) throws InterruptedException;
+    JSONObject readPodToBePlaced(String nodeName, long cpu, long memoryMB);
+    void cleanSystem(List<Node> nodes);
 }
