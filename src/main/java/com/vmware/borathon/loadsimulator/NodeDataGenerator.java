668400edca99ac8a9522701b92a92d7a32e82d92
Yash <ybhatnagar@vmware.com>
2018-04-30 14:44:24 +0530

    fixing nodedata generator data

--- src/main/java/com/vmware/borathon/loadsimulator/NodeDataGenerator.java
+++ src/main/java/com/vmware/borathon/loadsimulator/NodeDataGenerator.java
@@ -67,26 +67,26 @@ public class NodeDataGenerator {
 
     public static List<Node> generateFixedReal() {
         List<Node> kubeEnv = new ArrayList<>();
-        Node node = new Node("0", "first", 4000, 2000);
-        Pod pod1 = new Pod("0_0", "first_first", 600, 300,false);
+        Node node = new Node("0", "ip-172-20-0-120.ec2.internal", 3000, 1300);
+        Pod pod1 = new Pod("0_0", "first_first", 600, 100,false);
         Pod pod2 = new Pod("0_1", "first_second", 800, 400,false);
         Pod pod3 = new Pod("0_2", "first_third", 400, 200,false);
-        Pod pod4 = new Pod("0_3", "first_fourth", 300, 300,false);
-        Pod pod5 = new Pod("0_4", "first_fifth", 700, 300,false);
-        node.addPod(pod1); node.addPod(pod2); node.addPod(pod3); node.addPod(pod4); node.addPod(pod5);
+        Pod pod5 = new Pod("0_4", "first_fifth", 700, 100,false);
+
+        node.addPod(pod1); node.addPod(pod2); node.addPod(pod3); node.addPod(pod5);
         kubeEnv.add(node);
-        node = new Node("1", "second", 4000, 2000);
+        node = new Node("1", "ip-172-20-0-247.ec2.internal", 3750, 1500);
         pod1 = new Pod("1_0", "second_first", 500, 300,false);
         pod2 = new Pod("1_1", "second_second", 400, 200,false);
         pod3 = new Pod("1_2", "second_third", 800, 400,false);
-        pod4 = new Pod("1_3", "second_fourth", 1000, 300,false);
+        Pod pod4 = new Pod("1_3", "second_fourth", 1000, 300,false);
         node.addPod(pod1); node.addPod(pod2); node.addPod(pod3); node.addPod(pod4);
         kubeEnv.add(node);
-        node = new Node("2", "third", 4000, 2000);
-        pod1 = new Pod("2_0", "third_first", 1200, 700,false);
-        pod2 = new Pod("2_1", "third_second", 500, 300,false);
-        pod3 = new Pod("2_2", "third_third", 800, 400,false);
-        pod4 = new Pod("1_3", "third_fourth", 300, 300,false);
+        node = new Node("2", "ip-172-20-0-79.ec2.internal", 3180, 1400);
+        pod1 = new Pod("2_0", "third_first", 1200, 500,false);
+        pod2 = new Pod("2_1", "third_second", 500, 200,false);
+        pod3 = new Pod("2_2", "third_third", 800, 300,false);
+        pod4 = new Pod("1_3", "third_fourth", 300, 100,false);
         node.addPod(pod1); node.addPod(pod2); node.addPod(pod3); node.addPod(pod4);
         kubeEnv.add(node);
         return kubeEnv;
